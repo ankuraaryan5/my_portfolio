3145b5f0401fa17505b2d470819dda85ccc1429f
ankuraaryan5 <ankuraaryan5@gmail.com>
2025-02-02 09:33:53 +0530

    first print

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn1").addEventListener("click", function () {
+    window.open("https://drive.google.com/file/d/1G11kZraXYOtEDFKbkMnhfzV_5DYJ-2Un/view?usp=sharing", "_blank");
+  });
+  
+  document.getElementById("btn2").addEventListener("click", function () {
+    window.location.href = "mailto:ankuraaryan5@gmail.com?subject=Let's%20Connect";
+  });
